f70b10d25f582568ff289a455ba8b78ce54a6705
renat <renat@zaripov.(none)>
2013-08-07 23:19:05 +0400

    Modify voicecall

--- voicecall.c
+++ voicecall.c
@@ -1,8 +1,12 @@
 #include <gio/gio.h>
+#include <string.h>
+
 #include "modemd.h"
 #include "privdata.h"
 #include "voicecall.h"
 #include "debug.h"
+#include "u-boot.h"
+
 static void add_call(GDBusConnection *connection,
 		       const gchar *sender_name,
 		       const gchar *object_path,
@@ -12,6 +16,32 @@ static void add_call(GDBusConnection *connection,
 		       gpointer userdata)
 {
 	d_notice("AddCall: %s\n", g_variant_get_type_string(parameters));
+
+	GVariant *v;
+	//int i;
+	GVariantIter *iter;
+
+	char* str;
+	//g_print("eek!\n");
+	//i = 0;
+	///status.mode = 0;
+	//    g_variant_get(parameters, "(a{sv})", &iter);
+    //        while(g_variant_iter_loop(iter, "(sv)", &str, &v)) {
+	//	if (!strcmp(str, "mode"))
+	//		g_variant_get(v, "i", &status.mode);
+
+	char* number = fw_getenv("pongo_masternum");
+	d_notice("pongo_masternum: %s\n", number);
+
+	g_variant_get(parameters, "(a{sv})", &iter);
+
+	while(g_variant_iter_loop(iter, "(sv)", &str, &v)) {
+		if (!strcmp(str, "LineIdentification"))
+			d_notice("Get tel number\n");
+			//g_variant_get(v, "s", );
+	}
+
+	//if(strcmp(number,)
 }
 
 static void remove_call(GDBusConnection *connection,
